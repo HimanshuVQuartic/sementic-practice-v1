8b55fdcc18bfca4a7a4772dbf93279ea6c3fcb8c
HimanshuV <himanshu_v@quartic.ai>
2021-08-26 15:56:26 +0530

    master's 1st commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+ * function to generate random number
+ *
+ * @param min - minimum number to generate
+ * @param max - maximum number to generate
+ * @returns {number} the random number
+ */
+function randomNoGenerator(min, max) {
+  if(typeof(max) !== 'number' && typeof(min) !== 'number') {
+    min = 0;  max = 1;
+  }
+ return (Math.random() * (max-min)) + min;
+}
+
+
+
+module.exports = randomNoGenerator;
